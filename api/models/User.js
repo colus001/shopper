ccb554f5cf8d8fe39f627f1cb31be7a7d49c6857
Seokjun Kim <colus001@me.com>
2015-03-04 20:34:48 +0900

    First Commit

---
+++ api/models/User.js
@@ -0,0 +1,16 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  // DB 에 갑을 넣을 건데, 그걸 어떤 값을 넣을건지 정의를 해준다.
+
+  attributes: {
+
+  }
+};
+
